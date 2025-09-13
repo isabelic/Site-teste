1e8c14f043d046e76bd1d0b0cefba9bdaff04045
ISABELI MACHADO FERREIRA <isabeli_ferreira@estudante.sesisenai.org.br>
2025-09-12 21:45:19 -0300

    quase finalizado, ajuste no SDk e versao do emailJS

---
+++ js/script.js
@@ -0,0 +1,30 @@
+import * as emailjs from 'https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js';
+
+emailjs.init('yuw1FsmpWsA1ngbo3'); // sua Public Key
+
+const form = document.getElementById('feedbackForm');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
+
+  if (!email || !message) {
+    alert('Por favor, preencha seu e-mail e o feedback.');
+    return;
+  }
+
+  emailjs.send('service_8jb6pmh', 'template_scybpub', {
+    user_email: email,
+    message: message
+  })
+  .then(() => {
+    alert('Obrigado pelo seu feedback!');
+    form.reset();
+  })
+  .catch((error) => {
+    alert('Erro ao enviar, tente novamente mais tarde.');
+    console.error('EmailJS error:', error);
+  });
+});
